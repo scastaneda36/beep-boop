aafb08767d8ee57721ff094481aff070ada94522
Sandra.Castaneda <sancastans@gmail.com>
2018-10-26 15:42:24 -0700

    displays loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 function beepBoop(num){
-  var result = ""
-     var i;
+  var beeps = "beep!";
+  var boop = "boop!";
+  var dave = "I'm sorry, Dave. I'm afraid I can't do that.";
+  var array = [];
+  var i;
      for(i = 0; i < num; i++) {
+      if (i%3 === 0 && i > 0) {
+        array.push (dave);
+      } else if (array.includes ("1")) {
+        array.push (boop);
+      } else if (array.includes ("0")) {
+        array.push (beeps)
+      } else {
+        array.push (i)
+      }
+    }
 
-     }
-  //if (num.includes("0")){
-//return result
-  Output ((result += "The number is " +i));
+   return array;
+
 }
 $(document).ready(function() {
 $("#beepBoop").submit(function(event) {
